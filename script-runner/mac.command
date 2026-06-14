856804cc3f8237700c7654eae9af5d9a87a6bea6
Mike D <6719318+mikhaild908@users.noreply.github.com>
2026-06-14 14:03:39 -0700

    add script runners for mac, ubuntu, and windows

---
+++ script-runner/mac.command
@@ -0,0 +1,3 @@
+source ~/Scripts/.venv/bin/activate
+python3 ~/Scripts/the-script-to-run.py
+deactivate
---
+++ script-runner/win.bat
@@ -0,0 +1,4 @@
+@call %HOMEDRIVE%%HOMEPATH%\Scripts\.venv\Scripts\activate.bat
+@python %HOMEDRIVE%%HOMEPATH%\Scripts\the-script-to-run.py %*
+@pause
+@deactivate
